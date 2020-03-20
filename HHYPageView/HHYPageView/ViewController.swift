1cc99c98e0a4e53f2599e0526bf72d803544b736
huahuiyou <793316968@qq.com>
2020-03-20 16:07:38 +0800

    no message

---
+++ HHYPageView/HHYPageView/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  HHYPageView
+//
+//  Created by 华惠友 on 2020/3/18.
+//  Copyright © 2020 huayoyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1.标题
+        let titles = ["游戏", "娱乐娱乐娱乐", "新闻", "科学学", "技术", "游戏", "娱乐娱乐娱乐", "新闻", "科学学", "技术"]
+        let style = HHYTitleStyle()
+        style.isScrollEnable = true
+        style.isShowScrollLine = true
+        
+        // 2.子控制器
+        var childVcs = [UIViewController]()
+        for _ in 0..<titles.count {
+            let vc = UIViewController()
+            vc.view.backgroundColor = UIColor.randomColor()
+            childVcs.append(vc)
+        }
+        
+        // 3.pageView的frame
+        let pageFrame = CGRect(x: 0, y: heightOfAddtionalHeader + 64, width: view.bounds.width, height: view.bounds.height - (heightOfAddtionalHeader + 64))
+        
+        // 4.创建HHYPageView,并且添加到控制器的view中
+        let pageView = HHYPageView(frame: pageFrame, titles: titles, childVcs: childVcs, parentVc: self, style: style)
+        
+        view.addSubview(pageView)
+        
+        
+    }
+
+
+}
+
